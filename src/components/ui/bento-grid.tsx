85427cfa13007515c22cdb189e185dc290d6a456
sowrabb <svokkalkar@gmail.com>
2025-06-29 13:14:47 +0530

    feat: remove learn more from core features

--- src/components/ui/bento-grid.tsx
+++ src/components/ui/bento-grid.tsx
@@ -38,7 +38,7 @@ const BentoCard = ({
   Icon: React.ElementType;
   description: string;
   href: string;
-  cta: string;
+  cta?: string;
 }) => (
   <div
     key={name}
@@ -60,18 +60,25 @@ const BentoCard = ({
       <p className="max-w-lg text-neutral-400">{description}</p>
     </div>
 
-    <div
-      className={cn(
-        "pointer-events-none absolute bottom-0 flex w-full translate-y-10 transform-gpu flex-row items-center p-4 opacity-0 transition-all duration-300 group-hover:translate-y-0 group-hover:opacity-100",
-      )}
-    >
-      <Button variant="ghost" asChild size="sm" className="pointer-events-auto">
-        <a href={href}>
-          {cta}
-          <ArrowRight className="ml-2 h-4 w-4" />
-        </a>
-      </Button>
-    </div>
+    {cta && (
+      <div
+        className={cn(
+          "pointer-events-none absolute bottom-0 flex w-full translate-y-10 transform-gpu flex-row items-center p-4 opacity-0 transition-all duration-300 group-hover:translate-y-0 group-hover:opacity-100",
+        )}
+      >
+        <Button
+          variant="ghost"
+          asChild
+          size="sm"
+          className="pointer-events-auto"
+        >
+          <a href={href}>
+            {cta}
+            <ArrowRight className="ml-2 h-4 w-4" />
+          </a>
+        </Button>
+      </div>
+    )}
     <div className="pointer-events-none absolute inset-0 transform-gpu transition-all duration-300 group-hover:bg-black/[.03] group-hover:dark:bg-neutral-800/10" />
   </div>
 );
