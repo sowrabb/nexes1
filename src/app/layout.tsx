9705456806ad8e2f1dd39bd84b3fbbc3c699243d
sowrabb <svokkalkar@gmail.com>
2025-07-09 13:16:19 +0530

    Update Microsoft Clarity project ID to sc3qu8b79e

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,7 +34,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
         t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "sa8a6ent8t");`,
+    })(window, document, "clarity", "script", "sc3qu8b79e");`,
           }}
         />
         {/* End Microsoft Clarity */}
